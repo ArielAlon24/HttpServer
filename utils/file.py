0d9bd504909d8d39eff1c231fedbf753d3095427
Ariel Alon <arielalon@Ariels-MBP-2.Dlink>
2023-10-22 20:27:37 +0300

    Added file templates.

--- utils/file.py
+++ utils/file.py
@@ -6,3 +6,16 @@ def read(path: str) -> bytes:
         with open(path, "rb") as file:
             return file.read()
     return b""
+
+
+def template(path: str, **kwargs: str) -> bytes:
+    if not os.path.exists(path):
+        return b""
+
+    with open(path, "r") as template:
+        content = template.read()
+
+    for key, value in kwargs.items():
+        content = content.replace(f"[{key}]", value)
+
+    return content.encode()
